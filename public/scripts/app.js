e1606d8fa3e61759075e83b130f7c8676a1afb0d
kenjie <terradokenjie@gmail.com>
2021-09-24 14:02:59 +0800

    version-1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,28 +1,45 @@
-var app = angular.module('strawberry', ['ngSanitize']);
+strawberry.create('app',()=>{
 
-app.controller('mainCtrl',(
-        $templateRequest,
-        $sce,
-        $compile,
-        $scope,
-        $http,
-        $location
-    )=>{
-        $scope.profile = {
-            firstName:'Kenjie'
-        };
+    // Set pages if they require authorization or not
+    // If not set here, then the page will default to FALSE
+    var pages = {
+        '/': false,
+        '/index': false
+    }
+
+    // Requester object
+    var requester = {
+        isOnline:()=>{return null==localStorage.getItem("ractive")?"offline":localStorage.getItem("ractive")},
+
+        // Gets the name of the page the requester is currently in
+        onPage:()=>{return window.location.pathname.split('.')[0];},
+
+        isAuthorized:()=>{
+            let page = requester.onPage();
+            if (pages.hasOwnProperty(page) && pages[page]){
+                if (requester.isOnline()==='offline') {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    if (!requester.isAuthorized()) {
+        location.href='login.html';
+    }
+
+    // Set here how many seconds you'd like to page to appear
+    setTimeout(()=>{
+        document.getElementById('__strpage').style.display='block';
+        document.getElementById('__strloader').style.display='none';
+    },2000);
+
 });
 
+app.scope('main',($scope)=>{
 
+    // Declaration for  all the components to be used in your project
+    $scope.loader = '/components/loader.htm';
 
-app.component('profileCard',{
-    templateUrl: 'components/profile.htm',
-    controller:($scope)=>{
-        this.$onInit = function() {
-            console.log('asdasd');
-        };
-    },
-    bindings: {
-        data: '@'
-    }
 });
